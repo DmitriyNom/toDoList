d3daec78670c9f57e810b8cc5d2355159cd3fd90
Nomerovskiy Dmitriy <nomerovskiydv@gmail.com>
2023-11-27 12:58:47 +0500

    Adding fetch functions to get noteList

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddNoteForm from "./widgets/AddNoteForm";
 import ListWindow from "./widgets/ListWindow";
-import Note from "./components/Note";
+// import Note from "./components/Note";
 import './styles/App.css';
+import addressList from './addressList.js';
+
+// сделать page из этой страницы, а app вынести отдельным над-компонентом
 
 function App() {
 
-  const [notes, setNotes] = useState([
-    { id: 1, name: "First Note" },
-    { id: 2, name: "Second Note" },
-    { id: 3, name: "Third Note" },
-  ]);
+  const [notes, setNotes] = useState([]);
+
+  function printNotes() {
+    let currentAddress = addressList.get("getAllNotes");
+
+    let response = fetch(currentAddress)
+      .then(value => {
+        return value.json();
+      })
+      .then(value => {
+        setNotes(value)
+      })
+  }
+
+  printNotes();
+
+  let notesCounter = (notes.length === 0) ? 0 : notes[notes.length - 1].id;
+
 
   function addNote(newNote) {
     setNotes([...notes, newNote])
   }
 
   function deleteNote(id) {
-    setNotes(notes.filter(note => note.id != id));
+    setNotes(notes.filter(note => note.id !== id));
   }
 
   return (
     <div className="App">
       <ListWindow notes={notes} deleteNote={deleteNote} />
-      <AddNoteForm addNote={addNote} id={(notes[notes.length - 1].id) + 1} />
+      <AddNoteForm addNote={addNote} id={notesCounter + 1} />
     </div>
   );
 }
---
+++ src/addressList.js
@@ -0,0 +1,7 @@
+let addressList = new Map();
+
+addressList.set("getAllNotes", "http://localhost:8080/notes");
+
+
+export default addressList;
+
